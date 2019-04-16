e2d04224402e38bb89536805413b3d6195b2d820
MarioSimou <marios.simouu@gmail.com>
2019-04-16 21:59:59 +0100

    adding nav

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Switch , BrowserRouter , Route } from 'react-router-dom'
+
+// components
+import Home from '../Home'
+import Login from '../Login'
+import Navbar from '../Navbar'
 
 const App = props => {
     return(
-        <div>App</div>
+        <div className="app">
+            <BrowserRouter>
+                <Navbar />
+                <Switch>
+                    <Route exact path="/" component={ Home } />
+                    <Route exact path="/login" component={ Login } />
+                </Switch>
+            </BrowserRouter>
+        </div>
     )
 }
 
---
+++ frontend/src/components/Navbar/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './style.css'
+
+const Navbar = props => {
+    return (
+        <div className="nav">
+            <div className="left d-flex justify-content-center align-items-center">
+                <div>
+                    <i className="fas fa-code"></i>
+                </div>
+            </div>
+            <div className="center d-flex justify-content-start align-items-center">
+                <div className="field">
+                    <Link to="/">Home</Link>
+                </div>
+            </div>
+            <div className="right d-flex justify-content-end align-items-center">
+                <div className="field">
+                    <Link to="/login">Login</Link>
+                </div>
+                <div className="field">
+                    <Link to="/register">Register</Link>
+                </div>
+                <div className="field">
+                    <Link to="/logout">Logout</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
