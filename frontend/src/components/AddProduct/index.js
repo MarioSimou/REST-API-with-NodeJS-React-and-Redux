9a8e15e086db87e6c002c72e9b1beac322cbc48e
MarioSimou <marios.simouu@gmail.com>
2019-04-19 10:55:55 +0100

    fixing field

--- frontend/src/components/AddProduct/index.js
+++ frontend/src/components/AddProduct/index.js
@@ -6,7 +6,7 @@ const AddProduct = props => {
         <Header title="Add Product" 
                 content="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups." 
                 id="add-product"
-                />
+        />
     )
 }
 
