8427c498166cc3fbfc94f12d88a0087ac96e197b
MarioSimou <marios.simouu@gmail.com>
2019-04-21 21:24:13 +0100

    hadnling db errors

--- backend/controllers/main.js
+++ backend/controllers/main.js
@@ -18,14 +18,22 @@ router.post('/products' , validateAddProduct , async ( req , res ) => {
                 // store data to db
                 const q = 'INSERT INTO products( name , price , image , category , description ) VALUES ( $1 , $2 , $3 , $4 , $5 )'
                 await pg.query( q , Object.values(req.body));
-
+                // 23505 - error
                 // return a response
                 res.json({ statusCode : 200 , res : { content : 'The product has been successfully added.' , state: 'positive' }})
             } catch ( e ){
-                const error = new DatabaseError( e.detail )
+                let msg;
+                console.log( e )
                 // analyse errors
-                res.json({ statusCode : 500 , error : error.render() } )
-                // res.json({ statusCode : 500 , errors : { message : e.detail } })
+                switch( +e.code ){
+                    case 23505:
+                        msg = `Duplcicate product ${ e.constraint.match(/(?<=unique_products_)(.*)/g)[0] }`    
+                        break;
+                    default:
+                        msg = `${ e.detail }`
+                }
+                res.json({ statusCode : 500 , error : new DatabaseError( msg ).render() } )
+
             }
             break;
         default:
