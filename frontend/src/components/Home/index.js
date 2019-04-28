ceba424040a5874aebe9932b6eda5ddbcafa31f9
MarioSimou <marios.simouu@gmail.com>
2019-04-28 11:52:13 +0100

    auth functionality

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -2,7 +2,8 @@ import React , { useEffect } from 'react'
 import u from '../../util'
 import { connect } from 'react-redux'
 import { updateMessage , fetchProducts } from '../../actions'
-const Home = ({ message , fetchProducts , products }) => {
+
+const Home = ({ message , fetchProducts , products , userId }) => {
     const msgJSX = u.renderMessage( message )
     
     // effect hook- behaves like componentDidMount method 
@@ -10,16 +11,18 @@ const Home = ({ message , fetchProducts , products }) => {
         fetchProducts()
     }, [])
 
+
     switch( Object.values( products).length > 0 ? true : false ){
         case true:
         return (
             <React.Fragment>
                 { msgJSX }
                 <div className="home container-fluid p-0 m-0">
-                    <div className="container-fluid m-2">
+                    <div className="container-fluid p-2">
                         <div className="row">
                             { Object.values( products ).map( p => {
                                 console.log( p )
+
                                 return (
                                     <div className="col-12 col-md-6 col-lg-4" key={ p.product_id } >
                                         <div className="card">
@@ -28,17 +31,7 @@ const Home = ({ message , fetchProducts , products }) => {
                                                 <h5 className="card-title">{ p.product_name }</h5>
                                                 <h6 className="card-subtitle mb-2 text-muted">{ p.email }</h6>
                                                 <p className="card-text">{ p.description }</p>
-                                                <div>
-                                                    <a href={ `/products/${ p.product_id }` } className="mr-2 btn btn-outline-success" >
-                                                        View
-                                                    </a>
-                                                    <a href={ `/products/${ p.product_id }/edit` } className="mr-2 btn btn-outline-warning" >
-                                                        Edit
-                                                    </a>
-                                                    <a href="#" className="btn btn-outline-danger" >
-                                                        Delete
-                                                    </a>
-                                                </div>
+                                                { u.renderAuthButtons( userId , p ) }
                                             </div>
                                         </div>
                                     </div>
@@ -61,7 +54,7 @@ const Home = ({ message , fetchProducts , products }) => {
 }
 
 const mapStateToProps = state => {
-    return { message : state.messageReducer , products : state.requestsReducer }
+    return { message : state.messageReducer , products : state.requestsReducer , userId : state.userStatus }
 }
 
 export default connect( mapStateToProps  , { updateMessage , fetchProducts } )( Home )
